1fad6f8ef464d44868eacc2cbb1bc1a1dc2be728
Light1c3 <fullmetala@walla.com>
2018-02-16 09:33:57 -0500

    Inititial Commit

---
+++ js/calendarCards.js
@@ -0,0 +1,29 @@
+var Root = 'https://www.googleapis.com/calendar/v3/calendars/';
+var calendarID = '<YOUR CALENDAR ID HERE>';
+var maxResults = 2;
+var APIKey = '<YOUR KEY HERE>'; 
+var currectDate = (new Date()).toISOString();
+var EventsDiv = document.getElementById("EventCards");
+
+$(document).ready(function () {
+    $.ajax({
+      url: Root + calendarID + "events?maxResults=" + maxResults + "&timeMin=" + currectDate + "&singleEvents=true&orderBy=startTime" + '&key=' + APIKey,
+      method: 'GET'
+    }).then(function(data) {
+        var items = data.items;
+        for(var i = 0; i < items.length; i++) {
+            var startTime = new Date(items[i].start.dateTime);
+            var card = '<div class="card"><div class="container"><h4><b>' + items[i].summary + '</b></h4><br/><p>' + getShowDate(startTime) + '</p><p>' + getShowTime(startTime) + '</p><p>' + '</p></div></card>';
+            EventsDiv.innerHTML += card;      
+        }
+    });
+});
+
+function  getShowDate(date) {
+    var legth = (date.toUTCString().length - 13);
+    return "Date: " + date.toUTCString().substring(0,  legth);
+}
+
+function  getShowTime(date) {
+    return "Start Time: " + date.toLocaleTimeString();
+}
